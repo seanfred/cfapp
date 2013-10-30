34525888ba19bfd6dd89b4454e943529b7090964
seanfred <sean.frederick.1@gmail.com>
2013-10-29 21:20:46 -0700

    changed production email

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -14,12 +14,12 @@ class Devise::RegistrationsController < DeviseController
 
     if resource.save
       respond_to do |format|
-        format.html { 
+        format.html {
            if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_navigational_format?
           sign_up(resource_name, resource)
           respond_with resource, :location => after_sign_up_path_for(resource)
-         
+
           else
             set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
             expire_session_data_after_sign_in!
@@ -30,12 +30,12 @@ class Devise::RegistrationsController < DeviseController
       end
     else
       respond_to do |format|
-        format.html { 
+        format.html {
           clean_up_passwords resource
           respond_with resource
         }
-        format.json { 
-            render json: resource.errors, status: :unprocessable_entity 
+        format.json {
+            render json: resource.errors, status: :unprocessable_entity
 
         }
       end
