af506fa87bf5e957cf5540dcd05587ca3b1764b8
Michael Frankenfield <mfrank183@gmail.com>
2013-10-31 09:40:05 -0700

    roll back

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'factory_girl'
 
 describe AnswersController do
+  it 'sends a list of answers to create' do
+    FactoryGirl.create_list(:answer, 10)
 
+    get ''
+
+  end
 end
