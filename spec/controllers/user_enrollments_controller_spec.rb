587e23888a4b9bd074a0c9bae061156d9f58ade3
cheatermoves <lanners.marshall@yahoo.com>
2013-11-01 09:18:04 -0700

    devise test helper for rspec

--- spec/controllers/user_enrollments_controller_spec.rb
+++ spec/controllers/user_enrollments_controller_spec.rb
@@ -14,6 +14,7 @@ require 'factory_girl'
     describe "GET #new" do
       it "responds successfully with an HTTP 200 status code" do
         get :new
+        sign_in @user
         expect(response).to be_success
         expect(response.status).to eq(200)
       end
