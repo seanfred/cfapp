013cf3fcf335aab5645fc92000b032481b5456d9
Marshall Lanners <lanners.marshall@yahoo.com>
2013-11-01 09:41:38 -0700

    Update user_enrollments_controller_spec.rb

--- spec/controllers/user_enrollments_controller_spec.rb
+++ spec/controllers/user_enrollments_controller_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'factory_girl'
 
 
-<<<<<<< HEAD
   describe UserEnrollmentsController do
     describe "GET #index" do
       it "responds successfully with an HTTP 200 status code" do
@@ -11,33 +10,6 @@ require 'factory_girl'
         expect(response.status).to eq(200)
       end
     end
-=======
-#   describe UserEnrollmentsController do
-#     describe "GET #index" do
-#       it "responds successfully with an HTTP 200 status code" do
-#         get :index
-#         expect(response).to be_success
-#         expect(response.status).to eq(200)
-#       end
-#     end
-
-
-    # describe "GET #new" do
-    #   it "responds successfully with an HTTP 200 status code" do
-    #     get :new
-    #     sign_in @user
-    #     expect(response).to be_success
-    #     expect(response.status).to eq(200)
-    #   end
-    # end
-#     describe "GET #new" do
-#       it "responds successfully with an HTTP 200 status code" do
-#         get :new
-#         expect(response).to be_success
-#         expect(response.status).to eq(200)
-#       end
-#     end
->>>>>>> 429ebfaa447199020af21ac3d9f79cb4a6c29cf0
 
     describe "GET #new" do
       it "responds successfully with an HTTP 302 status code" do
